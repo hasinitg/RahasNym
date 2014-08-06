2070f7365f1104c959132d4d9635da8a9a3df6f3
Hasini Gunasinghe <hasi7786@gmail.com>
2014-08-06 08:33:57 +0530

    adding elements to lib

--- MySecuritySite/CryptoLib/src/main/java/org/crypto/lib/algorithms/ModularExponentiation.java
+++ MySecuritySite/CryptoLib/src/main/java/org/crypto/lib/algorithms/ModularExponentiation.java
@@ -12,11 +12,25 @@ public class ModularExponentiation {
 
     /**
      * Compute modular exponentiation through repeated squaring and multiplication.
+     *
+     *
      * @param number
      * @param exponent
+     * @param modulus
      * @return
      */
-    public BigInteger computeModularExponentiation(int number, int exponent) {
+    /*public BigInteger computeModularExponentiation(int number, int exponent, int modulus) {
+        //convert the exponent into binary
+        String exponentInBinary = Integer.toBinaryString(exponent);
+        //get the binary bits into an integer array
+        int binaryLength = exponentInBinary.length();
+        int[] binaryString = new int[binaryLength];
 
-    }
+        for (int i = binaryLength; i >= 0; i--) {
+            int index = 0;
+            String bit = binaryString[index] = exponentInBinary.substring(i, i-1);
+        }
+
+
+    }*/
 }
